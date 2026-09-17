28e60e0a581442a55ea712d2cfe93791b87e6860
leaeningenglishforzenshoezam <one.consistent@gmail.com>
2026-09-17 10:36:57 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const fadeElements = document.querySelectorAll(".fade");
+
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+
+      entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+
+          entry.target.classList.add("is-visible");
+
+          observer.unobserve(entry.target);
+        }
+
+      });
+
+    },
+    {
+      threshold: 0.15
+    }
+  );
+
+
+  fadeElements.forEach((element) => {
+    observer.observe(element);
+  });
+
+});
